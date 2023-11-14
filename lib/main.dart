c78549241974812c810a5e472552ee12f03363be
unknown <george.535220173@stu.untar.ac.id>
2023-11-14 19:05:14 +0700

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,14 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:project_uas_emoney/home/home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(myApp());
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class myApp extends StatelessWidget {
   @override
-  State<MyApp> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MyApp>{
-
-  @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Button Demo',
-      home: Scaffold(appBar: AppBar(title: Text('Button Demo'),),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: home()
     );
   }
 }
